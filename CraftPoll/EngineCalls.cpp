7a5ed46e768f6327e811592a095921b2d4ff4535
crvajacksonoliver <54992768+crvajacksonoliver@users.noreply.github.com>
2019-09-23 11:43:27 -0400

    Add files via upload

--- CraftPoll/EngineCalls.cpp
+++ CraftPoll/EngineCalls.cpp
@@ -2,9 +2,32 @@
 
 #define GM_EXPORT extern "C" __declspec (dllexport)
 
-GM_EXPORT void engine_block_registry_initialize()
+GM_EXPORT char* engine_block_registry_initialize()
 {
-	BlockRegistry::Initialize();
+	try
+	{
+		BlockRegistry::Initialize();
+	}
+	catch (std::exception ex)
+	{
+		char* result = (char*)malloc(2);
+		if (result == nullptr)
+			return nullptr;
+
+		result[0] = '0';
+		result[1] = '\0';
+
+		return result;
+	}
+
+	char* result = (char*)malloc(2);
+	if (result == nullptr)
+		return nullptr;
+
+	result[0] = '1';
+	result[1] = '\0';
+
+	return result;
 }
 
 GM_EXPORT double engine_block_registry_compile()
