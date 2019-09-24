1274ab4e10767a4cd3876e51ce156c6fefbc6df2
crvajacksonoliver <54992768+crvajacksonoliver@users.noreply.github.com>
2019-09-24 11:41:49 -0400

    Add files via upload

--- EnerverseVin/EnerverseVin.cpp
+++ EnerverseVin/EnerverseVin.cpp
@@ -14,7 +14,7 @@ class BlockDirt : public Block
 {
 public:
 	BlockDirt()
-		:Block(Material::EARTH, 1.0f, Tool::SHOVEL)
+		:Block("block_dirt", "Dirt", Material::EARTH, 1.0f, Tool::SHOVEL)
 	{
 
 	}
@@ -23,11 +23,63 @@ public:
 	{
 		return true;
 	}
+
+	char* OnBlockCreate(char* arguments) override
+	{
+		unsigned char argumentCount = 0;
+		unsigned short argumentsLength = strlen(arguments);
+		
+		for (unsigned short i = 0; i < argumentsLength; i++)
+		{
+			if (arguments[i] == ',')
+				argumentCount++;
+		}
+
+		if (argumentCount != 1)
+		{
+			char* metaData = (char*)malloc(1);
+			if (metaData == nullptr)
+				return nullptr;
+
+			metaData[0] = '\0';
+			return metaData;
+		}
+
+		char* metaData = (char*)malloc(argumentsLength);
+		if (metaData == nullptr)
+			return nullptr;
+
+		metaData[argumentsLength - 1] = '\0';
+
+		for (unsigned short i = 0; i < argumentsLength - 1; i++)
+		{
+			metaData[i] = arguments[i];
+		}
+
+		return metaData;
+	}
+
+	char* OnBlockUpdate(char* metaData) override
+	{
+		unsigned short metaLength = strlen(metaData);
+
+		for (signed short i = metaLength - 1; i > 0; i--)
+		{
+			if (metaData[i] == '9' && i != 0)
+			{
+				//HERE
+				metaData[i] = '0';
+				metaData[i - 1]++;
+			}
+		}
+
+		return metaData;
+	}
 };
 
 bool ModVin::InitializeModels()
 {
-	BlockTest block = BlockTest();
+	BlockDirt block = BlockDirt();
 	
 
 	BlockRegistry::RegisterBlock((Block*)&block);
